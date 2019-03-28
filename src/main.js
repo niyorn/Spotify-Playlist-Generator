dd80b85824391f7f88fca8e09bfcf9d6fe521370
niyorn <c.niyornram@hotmail.com>
2019-03-28 11:09:06 +0100

    update: Register loader component globally

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import LoadingIndicator from '@/components/LoadingIndicator'
+
+Vue.component('LoadingIndicator', LoadingIndicator)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  LoadingIndicator,
   render: h => h(App)
 }).$mount('#app')
